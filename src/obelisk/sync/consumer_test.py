8beff4c9a60104a8ce82c46417faf6a0f10a33f1
Stef Pletinck <stef.pletinck@ugent.be>
2025-03-25 14:09:25 +0100

    Test for double event loop case
    Creating several Consumers would crash,
    because multiple event loops would be created.
    We test for this bug so it does not emerge again

--- src/obelisk/sync/consumer_test.py
+++ src/obelisk/sync/consumer_test.py
@@ -11,3 +11,23 @@ def test_demo_igent():
     )
 
     assert len(result.items) == 2
+
+def test_two_instances():
+    consumer_one = Consumer(client="67c716e616c11421cfe2faf6", secret="08dafe89-0389-45b4-9832-cc565fb8c2eb")
+    consumer_two = Consumer(client="67c716e616c11421cfe2faf6", secret="08dafe89-0389-45b4-9832-cc565fb8c2eb")
+    result_one = consumer_one.single_chunk(
+        datasets=["612f6c39cbceda0ea9753d95"],
+        metrics=["org.dyamand.types.common.Temperature::number"],
+        from_timestamp=1740924034000,
+        to_timestamp=1741100614258,
+        limit=2
+    )
+    result_two = consumer_one.single_chunk(
+        datasets=["612f6c39cbceda0ea9753d95"],
+        metrics=["org.dyamand.types.common.Temperature::number"],
+        from_timestamp=1740924034000,
+        to_timestamp=1741100614258,
+        limit=2
+    )
+    assert len(result_one.items) == 2
+    assert len(result_two.items) == 2
