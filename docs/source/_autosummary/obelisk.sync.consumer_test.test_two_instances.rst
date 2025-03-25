82664289b73879e472c3bd8d8400c61828b85299
Stef Pletinck <stef.pletinck@ugent.be>
2025-03-25 14:35:20 +0100

    Doc incompatibility of sync module with async enviro
    One cannot use run_until_complete inside of a Future,
    this is a limitation of the asyncio design.
    
    Because our sync implementation is nothing but a run_until_complete
    wrapper around the asynchronous,
    it cannot be used in a Future environment.
    
    This catches especially users of Jupyter by surprise, so we document it.

---
+++ docs/source/_autosummary/obelisk.sync.consumer_test.test_two_instances.rst
@@ -0,0 +1,6 @@
+obelisk.sync.consumer\_test.test\_two\_instances
+================================================
+
+.. currentmodule:: obelisk.sync.consumer_test
+
+.. autofunction:: test_two_instances
--- src/obelisk/sync/__init__.py
+++ src/obelisk/sync/__init__.py
@@ -3,4 +3,11 @@ This module provides wrappers for the classes in `obelisk.asynchronous` with a s
 These hold on to a private event loop and block until a result is available.
 
 There is no synchronous alternative to `obelisk.asynchronous.client.Client`.
+
+Note
+----
+
+Please note that the sync module *will not* work in an asynchronous context (i.e. in any method returning a Future, or a Jupyter notebook).
+This is because it is internally nothing more than a wrapper over the asynchronous implementation.
+Use the asynchronous implementation in these situations.
 """
