bb777136b2698f48306a705e990bad8a7dff5a05
Stef Pletinck <stef.pletinck@ugent.be>
2025-06-02 09:55:24 +0200

    Fix test secrets

--- src/obelisk/asynchronous/consumer_test.py
+++ src/obelisk/asynchronous/consumer_test.py
@@ -3,9 +3,12 @@ from .consumer import Consumer
 
 pytest_plugins = ('pytest_asyncio',)
 
+client_id = "682c6c46604b3b3be35429df"
+client_secret = "7136832d-01be-456a-a1fe-25c7f9e130c5"
+
 @pytest.mark.asyncio
 async def test_demo_igent():
-    consumer = Consumer(client="67c716e616c11421cfe2faf6", secret="08dafe89-0389-45b4-9832-cc565fb8c2eb")
+    consumer = Consumer(client=client_id, secret=client_secret)
     result = await consumer.single_chunk(
         datasets=["612f6c39cbceda0ea9753d95"],
         metrics=["org.dyamand.types.common.Temperature::number"],
